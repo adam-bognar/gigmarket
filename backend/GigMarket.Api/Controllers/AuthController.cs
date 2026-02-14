d17879b63fd164f54051a190636c498a742b64c4
Ádám Bognár <bgnr.adam@gmail.com>
2026-02-14 13:24:39 +0100

    Add AuthController skeleton
    Introduce a new AuthController in GigMarket.Api.Controllers with route "api/[controller]". The controller injects SignInManager<User> and UserManager<User> via constructor to prepare for authentication endpoints; no action methods have been implemented yet.

---
+++ backend/GigMarket.Api/Controllers/AuthController.cs
@@ -0,0 +1,21 @@
+﻿using GigMarket.Domain.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GigMarket.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly SignInManager<User> _signInManager;
+        private readonly UserManager<User> _userManager;
+
+        public AuthController(SignInManager<User> signInManager, UserManager<User> userManager)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+    }
+}
