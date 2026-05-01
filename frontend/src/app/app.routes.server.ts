a95f02d910a51efdaf34b9b2e0aaf8c94fb58141
Ádám Bognár <bgnr.adam@gmail.com>
2026-05-01 18:25:03 +0200

    Add orders route and navbar links

--- frontend/src/app/app.routes.server.ts
+++ frontend/src/app/app.routes.server.ts
@@ -1,8 +1,20 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
-  {
-    path: '**',
-    renderMode: RenderMode.Prerender
-  }
+  { path: '', renderMode: RenderMode.Prerender },
+  { path: 'browse', renderMode: RenderMode.Prerender },
+  { path: 'login', renderMode: RenderMode.Prerender },
+  { path: 'become', renderMode: RenderMode.Prerender },
+  { path: 'gigs/:id', renderMode: RenderMode.Server },
+
+  { path: 'orders', renderMode: RenderMode.Client },
+  { path: 'orders/**', renderMode: RenderMode.Client },
+  { path: 'dashboard/**', renderMode: RenderMode.Client },
+  { path: 'inbox', renderMode: RenderMode.Client },
+  { path: 'inbox/**', renderMode: RenderMode.Client },
+  { path: 'account', renderMode: RenderMode.Client },
+  { path: 'create-gig', renderMode: RenderMode.Client },
+  { path: 'create-gig/:id', renderMode: RenderMode.Client },
+
+  { path: '**', renderMode: RenderMode.Server },
 ];
